e7a9498d79e57c2c5345e5e4fd97b28a9549cfcf
Paras <mehtaparas1901@gmail.com>
2024-02-03 20:17:50 +0530

    whiteboard created

---
+++ Client/src/Components/WhiteBoard/index.jsx
@@ -0,0 +1,11 @@
+
+
+const index = () => {
+  return (
+    <canvas className="h-100vh w-full border border-red-300 max-w-[70%] mx-auto my-10">
+      hhjfj
+    </canvas>
+  )
+}
+
+export default index

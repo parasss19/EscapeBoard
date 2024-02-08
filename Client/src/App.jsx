31ed970d1236bd0d63e6124a79de6561e674f16c
Paras <mehtaparas1901@gmail.com>
2024-02-08 14:13:22 +0530

    routing modified

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,1 +1,27 @@
-//No need we write all routing in main.jsx
+import React from 'react'
+
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+import Layout from "./Layout";
+import HomePage from "../src/Components/HomePage";
+import Forms from "../src/Components/Forms";         // For join/create room
+import RoomPage from "../src/Components/RoomPage";   //WhiteBoard
+
+const App = () => {
+
+  return (
+    <Router>
+      <Routes>
+
+        <Route path="/" element={<Layout />}>
+          <Route path='' element={<HomePage />} />
+          <Route path="room" element={<Forms />} />
+          <Route path="room/:roomId" element={<RoomPage />} />
+        </Route>
+
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,52 +1,12 @@
 import React from 'react'
+import App from './App'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'     //RouterProvider component use router prop.... and To create router we import one method "createBrowserRouter" which take array of objects as parameter
-
-import Layout from './Layout'
-import HomePage from '../src/Components/HomePage'
-import Forms from "../src/Components/Forms";
-import RoomPage from '../src/Components/RoomPage'
-
-
-//creating router
-const router = createBrowserRouter([
-  {
-    path : '/',
-    element: <Layout/>,
-
-    //Now our routes for diff pages
-    children: [
-      {
-        path: "",
-        element: <HomePage />
-      },
-      {
-        path: "room",
-        element: <Forms />
-      },
-      {
-        path: "room/:roomId",
-        element: <RoomPage />
-      },
-      
-      // {
-      //   path: "contact",
-      //   element: <Contact />
-      // },
-      // {
-      //   path:"user/:userid",     //here we want to show the params from the url and showed them in our website dynamically 
-      //   element: <User />
-      // },
-
-    ]
-  }
-])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider  router={router}/>
+   <App/>
   </React.StrictMode>,
 )
 
